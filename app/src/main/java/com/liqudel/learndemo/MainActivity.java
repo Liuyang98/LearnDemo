725331f85239f78dea5b7611a731f167409a6e80
liuyang <449360145@qq.com>
2018-09-10 10:26:58 +0800

    贪心算法

--- app/src/main/java/com/liqudel/learndemo/MainActivity.java
+++ app/src/main/java/com/liqudel/learndemo/MainActivity.java
@@ -12,7 +12,12 @@ import com.liqudel.learndemo.adapter.SamplePagerAdapter;
 import com.liqudel.learndemo.bean.ValueSendBean;
 import com.liqudel.learndemo.fragment.AlgorithmFragment;
 import com.liqudel.learndemo.fragment.SimpleFragment;
+import com.liqudel.learndemo.leetcode.Subject198;
 import com.liqudel.learndemo.leetcode.Subject263;
+import com.liqudel.learndemo.leetcode.Subject55;
+import com.liqudel.learndemo.leetcode.Subject62;
+import com.liqudel.learndemo.leetcode.Subject64;
+import com.liqudel.learndemo.leetcode.Subject75;
 import com.liqudel.learndemo.proxy.DynamicProxy;
 import com.liqudel.learndemo.proxy.RealSubject;
 import com.liqudel.learndemo.proxy.Subject;
@@ -48,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 //        valueTest();
 //        proxyTest();
 
-     new Subject263().run();
+        new Subject55().run();
     }
 
 
--- app/src/main/java/com/liqudel/learndemo/leetcode/Subject198.java
+++ app/src/main/java/com/liqudel/learndemo/leetcode/Subject198.java
@@ -5,17 +5,13 @@ import android.util.Log;
 
 /**
  * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
- * <p>
  * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
- * <p>
  * 示例 1:
- * <p>
  * 输入: [1,2,3,1]
  * 输出: 4
  * 解释: 偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
  * 偷窃到的最高金额 = 1 + 3 = 4 。
  * 示例 2:
- * <p>
  * 输入: [2,7,9,3,1]
  * 输出: 12
  * 解释: 偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
@@ -26,33 +22,39 @@ public class Subject198 implements Subjcet {
 
     @Override
     public void run() {
-        int n = rob(new int[]{1, 2, 3, 4});
+        int n = rob(new int[]{2, 7, 9, 11, 1});
         Log.e(TAG, "run: " + n);
     }
 
     private int rob(int[] nums) {
-        int p[] = new int[nums.length];
-        p[0] = nums[0];
-        p[1] = nums[1];
-
-
-//        for (int i = 2; i < nums.length; i++) {
-//
-//
-//
-//            int max = Math.max(nums[i - 1], nums[i - 2]);
-//            p[i] = nums[i] + max;
-//            Log.e(TAG, "i   ：" + i + "  p[i] ：：" + p[i]);
-//        }
-
-        int index=-1;
-        while (index<nums.length){
-
-
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
 
+        // 如果数组中的元素个个数大于2个，对于【A】式，i=2，dp[2-3]不存在
+        if (nums.length > 2) {
+            nums[2] += nums[0];
+        }
 
+        // 从第四个元素开始处理
+        int i = 3;
+        for (; i < nums.length; i++) {
+            // 求出第i个元素的最大值
+            nums[i] += Math.max(nums[i - 2], nums[i - 3]);
+        }
 
-        return p[p.length - 1];
+        // 如果只有一个元素，返回这个元素值
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        // 有两个元素返回其中较大的值
+        else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        // 多于两个元素，最大值在末尾两个之间，找最大的返回
+        else {
+            return Math.max(nums[i - 1], nums[i - 2]);
+        }
+
     }
 }
